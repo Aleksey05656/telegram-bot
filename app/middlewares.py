c5e4b2d9cae1de0abc0087f4fa84cb1b6d6c0332
Aleksey05656 <aleksey-seller@yandex.ru>
2025-09-09 11:36:27 +0500

    feat(app): migrate configs to Pydantic v2

---
+++ app/middlewares.py
@@ -0,0 +1,30 @@
+"""
+@file: middlewares.py
+@description: Basic FastAPI middlewares
+@dependencies: fastapi
+@created: 2025-09-09
+"""
+
+import time
+from typing import Callable
+
+from fastapi import Request
+from starlette.middleware.base import BaseHTTPMiddleware
+
+
+class ProcessingTimeMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next: Callable):
+        start = time.perf_counter()
+        response = await call_next(request)
+        response.headers["X-Process-Time"] = str(time.perf_counter() - start)
+        return response
+
+
+class RateLimitMiddleware(BaseHTTPMiddleware):
+    def __init__(self, app, requests: int, per_seconds: int):
+        super().__init__(app)
+        self.requests = requests
+        self.per_seconds = per_seconds
+
+    async def dispatch(self, request: Request, call_next: Callable):
+        return await call_next(request)
