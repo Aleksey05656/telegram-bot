b9cb5d9f0e612631b0be3fdfe7e363f703054826
Aleksey05656 <aleksey-seller@yandex.ru>
2025-10-01 10:10:32 +0500

    fix(telegram): expose package exports for worker imports

---
+++ telegram/__init__.py
@@ -0,0 +1,12 @@
+"""Telegram bot package with middlewares, handlers and services."""
+
+__all__ = [
+    "bot",
+    "dependencies",
+    "handlers",
+    "middlewares",
+    "models",
+    "services",
+    "utils",
+    "widgets",
+]
